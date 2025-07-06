59c95e082e734c0593905c14e463191ed29ba2bf
Mihai Dumitrescu <mihairazvand1@gmail.com>
2025-07-06 22:40:19 +0300

    Add base class for LightingChain
    Signed-off-by: Mihai Dumitrescu <mihairazvand1@gmail.com>

--- src/TowerDefense/tower/ThunderRod.cpp
+++ src/TowerDefense/tower/ThunderRod.cpp
@@ -6,7 +6,8 @@ static float distance(const sf::Vector2f& a, const sf::Vector2f& b) {
 }
 
 ThunderRod::ThunderRod(const sf::Vector2f& position, std::vector<Projectile>& projectiles, std::vector<Enemy>& enemies)
-    : Tower(position, projectiles), mMaxChains(3), mChainRange(100.f), mEnemies(enemies) {
+    : Tower(position, projectiles), mMaxChains(3), mChainRange(100.f), mEnemies(enemies),
+      mFireCooldown(1.25f), mProjectileSpeed(200.0f), mPosition(position), mDamage(5) {
     mShape.setFillColor(sf::Color::Cyan);
 
     mRange = 120.0f;
@@ -20,3 +21,27 @@ ThunderRod::ThunderRod(const sf::Vector2f& position, std::vector<Projectile>& pr
     // mUpgrades.push_back(Upgrade("Chain Count", 300, 1, [this]() { mMaxChains++; }));
 }
 
+void ThunderRod::update(float dt, std::vector<Enemy>& enemies)
+{
+    mFireCooldown -= dt;
+    if (mFireCooldown <= 0.0f) {
+        Enemy* closestEnemy = nullptr;
+        float closestDistance = mRange;
+
+        for (auto& enemy : enemies) {
+            float distance = std::hypot(enemy.getPosition().x - mPosition.x, enemy.getPosition().y - mPosition.y);
+            if (distance < closestDistance) {
+                closestEnemy = &enemy;
+                closestDistance = distance;
+            }
+        }
+
+        if (closestEnemy != nullptr) {
+            mProjectiles.emplace_back(LightingChain(mPosition, closestEnemy, mProjectileSpeed, mDamage));
+            mFireCooldown = mFireRate;
+        }
+    }
+
+    for (auto& projectile : mProjectiles)
+        projectile.update(dt);
+}
