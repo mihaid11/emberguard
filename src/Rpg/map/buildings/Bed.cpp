f473984ca804e1dad519d1532b82ceab1bcc4a3e
Mihai Dumitrescu <mihairazvand1@gmail.com>
2025-07-29 02:42:50 +0300

    Introduce TransitionSystem into Bed class to handle the sleep method
    Signed-off-by: Mihai Dumitrescu <mihairazvand1@gmail.com>

--- src/Rpg/map/buildings/Bed.cpp
+++ src/Rpg/map/buildings/Bed.cpp
@@ -3,15 +3,61 @@
 #include "../../../GameManager.h"
 
 Bed::Bed(const sf::Vector2f& position, float sizeIncrement, const std::string filename,
-         const sf::Vector2f& collPosition, const sf::Vector2f& collSize, const sf::Vector2f& interactPosition,
-         const sf::Vector2f& interactSize, TimeSystem& timeSystem, GameManager* gameManager)
+         const sf::Vector2f& collPosition, const sf::Vector2f& collSize,
+         const sf::Vector2f& interactPosition, const sf::Vector2f& interactSize,
+         TimeSystem& timeSystem, TransitionSystem& transitionSystem,
+         GameManager* gameManager)
          : Entity(position, filename, collPosition, collSize, 1, interactPosition, interactSize),
-           mTimeSystem(timeSystem), mGameManager(gameManager) {
+        mTimeSystem(timeSystem), mGameManager(gameManager), mCollSize(collSize),
+        mTransitionSystem(transitionSystem), mCollPosition(collPosition) {
     mSprite.setScale(sf::Vector2f(sizeIncrement, sizeIncrement));
+
+    if (!mFont.loadFromFile("assets/fonts/gameFont.ttf"))
+        std::cerr << "Failed to load font for AnalyzeMenu!" << std::endl;
+
+    mErrorText.setFillColor(sf::Color::White);
+    mErrorText.setFont(mFont);
+    mErrorText.setCharacterSize(12);
+    mErrorText.setString("Too early to sleep!");
+    mErrorText.setPosition(sf::Vector2f(collPosition.x + collSize.x / 2.f - 58.f,
+                                        collPosition.y - 30.f));
 }
 
 void Bed::interact() {
-    std::cout << "Good night!" << std::endl;
-    mGameManager->startTransition(true);
+    if (mTimeSystem.getHour() < 6 || mTimeSystem.getHour() >= 21) {
+        mTransitionSystem.startSleepTransition(
+        sf::Vector2f(930.f, 300.f),
+        1.25f,
+        [this]() { return (mTimeSystem.getHour() >= 21 || mTimeSystem.getHour() < 6); },
+        [this]() { mTimeSystem.setTimeScale(0.37f); }
+        );
+
+        mTimeSystem.setTimeScale(0.000004f);
+    } else {
+        mErrorText.setPosition(sf::Vector2f(mCollPosition.x + mCollSize.x / 2.f - 58.f,
+                                            mCollPosition.y - 30.f));
+        mErrorText.setString("Too early to sleep!");
+        mErrorText.setFillColor(sf::Color::White);
+        mClock.restart();
+        mShowText = true;
+    }
+}
+
+void Bed::render(sf::RenderWindow& window) {
+    // If the error text is visible gradually make it dissapear and render it
+    if (mShowText) {
+        float elapsedTime = mClock.getElapsedTime().asSeconds();
+        if (elapsedTime > 1.8f) {
+            mShowText = false;
+            mErrorText.setString("");
+        }
+        else {
+            int alpha = static_cast<int>(255 * (1.0f - (elapsedTime / 1.8f)));
+            mErrorText.setFillColor(sf::Color(255, 255, 255, alpha));
+        }
+        window.draw(mErrorText);
+    }
+
+    window.draw(mSprite);
 }
 
--- src/Rpg/map/buildings/Bed.h
+++ src/Rpg/map/buildings/Bed.h
@@ -2,6 +2,7 @@
 #include "Entity.h"
 #include <SFML/Graphics.hpp>
 #include "../../../TimeSystem.h"
+#include "../../../TransitionSystem.h"
 
 class GameManager;
 
@@ -9,13 +10,25 @@ class Bed : public Entity {
 public:
     Bed(const sf::Vector2f& position, float sizeIncrement, const std::string filename,
         const sf::Vector2f& collPosition, const sf::Vector2f& collSize,
-        const sf::Vector2f& interactPosition, const sf::Vector2f& interactSize, TimeSystem& timeSystem,
+        const sf::Vector2f& interactPosition, const sf::Vector2f& interactSize,
+        TimeSystem& timeSystem, TransitionSystem& transitionSystem,
         GameManager* gameManager);
 
     void interact() override;
+    void render(sf::RenderWindow& window) override;
 
 private:
     TimeSystem& mTimeSystem;
+    TransitionSystem& mTransitionSystem;
     GameManager* mGameManager;
+
+    sf::Vector2f mCollPosition;
+    sf::Vector2f mCollSize;
+
+    // Rendering of the error message variables
+    sf::Font mFont;
+    bool mShowText;
+    sf::Text mErrorText;
+    sf::Clock mClock;
 };
 
