b8cf3d47363df183fa8f23c1870904eebd055538
mihaid11 <mihairazvand1@gmail.com>
2025-10-05 00:20:03 +0300

    Implement item removal/addition through dialogue actions
    Signed-off-by: mihaid11 <mihairazvand1@gmail.com>

--- src/Rpg/dialogueSystem/DialogueActionExecutor.cpp
+++ src/Rpg/dialogueSystem/DialogueActionExecutor.cpp
@@ -1,5 +1,11 @@
 #include "DialogueActionExecutor.h"
 #include "../gamengine/RPGEngine.h"
+#include "../inventory/items/Item.h"
+#include "../inventory/items/TowerBlueprint.h"
+#include "../inventory/items/TowerBlueprintRare.h"
+#include "../inventory/items/TowerBlueprintEpic.h"
+#include "../inventory/items/TowerBlueprintMythic.h"
+#include "../inventory/items/Wood.h"
 #include <iostream>
 
 DialogueActionExecutor::DialogueActionExecutor(StoryManager& storyManager, RPGEngine& rpgEngine, Inventory& inventory)
@@ -18,9 +24,35 @@ void DialogueActionExecutor::executeAction(const DialogueAction& action) {
     } else if (action.type == "set_menu_flag") {
         mRpgEngine.setFlag(action.target, action.value);
     } else if (action.type == "give_item") {
-        // Implement item addition
+        std::unique_ptr<Item> mItem;
+        if (action.target == "TowerBlueprint")
+            mItem = std::make_unique<TowerBlueprint>();
+        else if (action.target == "TowerBlueprintRare")
+            mItem = std::make_unique<TowerBlueprintRare>();
+        else if (action.target == "TowerBlueprintEpic")
+            mItem = std::make_unique<TowerBlueprintEpic>();
+        else if (action.target == "TowerBlueprintMythic")
+            mItem = std::make_unique<TowerBlueprintMythic>();
+        else if (action.target == "Wood")
+            mItem = std::make_unique<Wood>();
+
+        if (mItem)
+            mInventory.addItem(std::move(mItem), action.value);
     } else if (action.type == "remove_item") {
-        // Implement item removal
+        int itemId = -1;
+        if (action.target == "TowerBlueprint")
+            itemId = TowerBlueprint().getId();
+        else if (action.target == "TowerBlueprintRare")
+            itemId = TowerBlueprintRare().getId();
+        else if (action.target == "TowerBlueprintEpic")
+            itemId = TowerBlueprintEpic().getId();
+        else if (action.target == "TowerBlueprintMythic")
+            itemId = TowerBlueprintMythic().getId();
+        else if (action.target == "Wood")
+            itemId = Wood().getId();
+
+        if (itemId != -1)
+            mInventory.removeItemById(itemId, action.value);
     } else if (action.type == "change_crystals") {
         mRpgEngine.changeCrystals(action.value);
     } else if (action.type == "set_chapter") {
