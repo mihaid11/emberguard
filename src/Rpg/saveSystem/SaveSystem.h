16e3508072c6cccff893cb29779bdce94354681d
mihaid11 <mihairazvand1@gmail.com>
2025-10-21 00:21:28 +0300

    Save player animation
    Signed-off-by: mihaid11 <mihairazvand1@gmail.com>

--- src/Rpg/saveSystem/SaveSystem.h
+++ src/Rpg/saveSystem/SaveSystem.h
@@ -9,7 +9,8 @@ class SaveSystem {
 public:
     SaveSystem();
 
-    void save(const sf::Vector2f& playerPosition, const std::vector<sf::Vector2f>& npcPositions, const std::vector<int>& npcWaypoints,
+    void save(const sf::Vector2f& playerPosition, const int& playerAnimation,
+              const std::vector<sf::Vector2f>& npcPositions, const std::vector<int>& npcWaypoints,
               const int& crystals, const int& year, const int& day, const int& hour,
               const int& minute, const int& bankBalance, const int& hasBorrowActive,
               const int& penalty, const int& interest, const int& amountToRepay, const int& daysToRepayment,
@@ -24,7 +25,8 @@ public:
               const int& chapter, const std::vector<std::string>& flagKeys,
               const std::vector<int>& flagValues);
 
-    bool load(sf::Vector2f& playerPosition, std::vector<sf::Vector2f>& npcPositions, std::vector<int>& npcWaypoints,
+    bool load(sf::Vector2f& playerPosition, int& playerAnimation,
+              std::vector<sf::Vector2f>& npcPositions, std::vector<int>& npcWaypoints,
               int& crystals, int& year, int& day, int& hour, int& minute, int& bankBalance,
               int& hasBorrowActive, int& penalty, int& interest, int& amountToRepay, int& daysToRepayment,
               int& startYear, int& startDay, int& startHour, int& startMinute,
