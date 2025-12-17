6e37e96202e0ae14f5a5e3c817887fa0003c32eb
Mihai Dumitrescu <mihairazvand1@gmail.com>
2025-12-17 16:30:45 +0200

    Implement callbacks for map change in the MCHouse
    Signed-off-by: Mihai Dumitrescu <mihairazvand1@gmail.com>

--- src/Rpg/map/buildings/MCHouseInt.cpp
+++ src/Rpg/map/buildings/MCHouseInt.cpp
@@ -4,17 +4,17 @@
 MCHouseInt::MCHouseInt(const sf::Vector2f& position, const std::string filename, const sf::Vector2f& collPosition,
                        const sf::Vector2f& collSize, int canInteract, const sf::Vector2f& interactPos,
                        const sf::Vector2f& interactSize, MainCharacter& mainCharacter, bool& isInsideAStructure,
-                       sf::Vector2f& cameraFixedPosition)
+                       sf::Vector2f& cameraFixedPosition, std::function<void(const std::string&)> changeMap)
     : Entity(position, filename, collPosition, collSize, canInteract, interactPos, interactSize), mMainCharacter(mainCharacter),
-    mIsInsideAStructure(isInsideAStructure), mCameraFixedPosition(cameraFixedPosition) {
-    mSprite.setScale(sf::Vector2f(1.37f, 1.37f));
+    mIsInsideAStructure(isInsideAStructure), mCameraFixedPosition(cameraFixedPosition), mChangeMap(changeMap) {
 }
 
 void MCHouseInt::interact() {
-    std::cout << "Teleporting MC from house!" << std::endl;
-    mMainCharacter.setPosition(sf::Vector2f(340.f, 538.f));
+    mMainCharacter.setPosition(sf::Vector2f(295.f, 262.f));
     mMainCharacter.setAnimation(4);
     mIsInsideAStructure = false;
     mCameraFixedPosition = { 0.f, 0.f };
+
+    mChangeMap("open_world");
 }
 
