264ef33f18d7ae0e077181e71c74ca2d574b8d76
Mihai Dumitrescu <mihairazvand1@gmail.com>
2025-12-17 16:30:45 +0200

    Implement callbacks for map change in the MCHouse
    Signed-off-by: Mihai Dumitrescu <mihairazvand1@gmail.com>

--- src/Rpg/map/buildings/MCHouse.cpp
+++ src/Rpg/map/buildings/MCHouse.cpp
@@ -4,17 +4,18 @@
 MCHouse::MCHouse(const sf::Vector2f& position, const std::string filename, const sf::Vector2f& collPosition,
                  const sf::Vector2f& collSize, int canInteract, const sf::Vector2f& interactPos,
                  const sf::Vector2f& interactSize, MainCharacter& mainCharacter, bool& isInsideAStructure,
-                 sf::Vector2f& cameraFixedPosition)
+                 sf::Vector2f& cameraFixedPosition, std::function<void(const std::string&)> changeMap)
     : Entity(position, filename, collPosition, collSize, canInteract, interactPos, interactSize), mMainCharacter(mainCharacter),
-    mIsInsideAStructure(isInsideAStructure), mCameraFixedPosition(cameraFixedPosition) {
+    mIsInsideAStructure(isInsideAStructure), mCameraFixedPosition(cameraFixedPosition), mChangeMap(changeMap) {
 
 }
 
 void MCHouse::interact() {
-    std::cout << "Teleporting MC to house!" << std::endl;
-    mMainCharacter.setPosition(sf::Vector2f(-912.f, -638.f));
+    mMainCharacter.setPosition(sf::Vector2f(77.f - 1024.f, 290.f - 1024.f));
     mMainCharacter.setAnimation(3);
     mIsInsideAStructure = true;
-    mCameraFixedPosition = { -760.f, -760.f };
+    mCameraFixedPosition = { 240.f - 1024.f, 192.5f - 1024.f };
+
+    mChangeMap("assets/maps/house_interior.json");
 }
 
--- src/Rpg/map/buildings/MCHouse.h
+++ src/Rpg/map/buildings/MCHouse.h
@@ -1,5 +1,6 @@
 #pragma once
-#include "Entity.h"
+#include <functional>
+#include "../entities/Entity.h"
 #include "../../mainCharacter/MainCharacter.h"
 
 class MCHouse : public Entity {
@@ -7,12 +8,15 @@ public:
     MCHouse(const sf::Vector2f& position, const std::string filename,
         const sf::Vector2f& collPosition, const sf::Vector2f& collSize, int canInteract,
         const sf::Vector2f& interactPos, const sf::Vector2f& interactSize,
-        MainCharacter& mainCharacter, bool& isInsideAStructure, sf::Vector2f& cameraFixedPosition);
+        MainCharacter& mainCharacter, bool& isInsideAStructure, sf::Vector2f& cameraFixedPosition,
+        std::function<void(const std::string&)> changeMap);
 
     void interact() override;
 private:
     MainCharacter& mMainCharacter;
     bool& mIsInsideAStructure;
     sf::Vector2f& mCameraFixedPosition;
+
+    std::function<void(const std::string&)> mChangeMap;
 };
 
