85ddfebbe8dd92e19963be4ee05992c80aba1d2e
Mihai Dumitrescu <mihairazvand1@gmail.com>
2025-12-22 20:17:31 +0200

    Add chest sprite
    Signed-off-by: Mihai Dumitrescu <mihairazvand1@gmail.com>

--- assets/maps/tilesets/Furniture.tsx
+++ assets/maps/tilesets/Furniture.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Furniture" tilewidth="82" tileheight="34" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="Furniture" tilewidth="82" tileheight="43" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1">
   <image source="../../sprites/buildings/bed.png" width="82" height="34"/>
+ </tile>
+ <tile id="2">
+  <image source="../../sprites/buildings/chest.png" width="60" height="43"/>
  </tile>
 </tileset>
