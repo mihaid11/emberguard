38868bf992e8f3a3de2aa29ad4ba3fdbace4adef
Mihai Dumitrescu <mihairazvand1@gmail.com>
2026-03-13 21:34:15 +0200

    Implement level system
    Signed-off-by: Mihai Dumitrescu <mihairazvand1@gmail.com>

---
+++ src/Rpg/mainCharacter/LevelSystem.cpp
@@ -0,0 +1,48 @@
+#include "LevelSystem.h"
+#include <iostream>
+#include "../inventory/items/TowerBlueprintEpic.h"
+
+
+LevelSystem::LevelSystem(Inventory* inventory, int maxLevel)
+    : mInventory(inventory), mCurrentXp(0), mCurrentLevel(0), mMaxLevel(maxLevel) {
+
+}
+
+void LevelSystem::addXp(int xp) {
+    if (mCurrentLevel < mMaxLevel) {
+        mCurrentXp += xp;
+        checkLevelUp();
+    }
+}
+
+void LevelSystem::checkLevelUp() {
+    while (mCurrentLevel < mMaxLevel && mCurrentXp >= getXpForNextLevel()) {
+        mCurrentXp -= getXpForNextLevel();
+        mCurrentLevel++;
+
+        grantRewards(mCurrentLevel);
+    }
+}
+
+void LevelSystem::grantRewards(int level) {
+    std::cout << "Leveled up!" << std::endl;
+}
+
+int LevelSystem::getCurrentXp() const {
+    return mCurrentXp;
+}
+
+int LevelSystem::getLevel() const {
+    return mCurrentLevel;
+}
+
+int LevelSystem::getXpForNextLevel() const {
+    return static_cast<float>(100.f * std::pow(mCurrentLevel, 1.25f));
+}
+
+float LevelSystem::getXpPercentage() const {
+    if (mCurrentLevel >= mMaxLevel)
+        return 1.f;
+
+    return static_cast<float>(mCurrentXp) / static_cast<float>(getXpForNextLevel());
+}
