2736f2ce5daa818e38e648739a11158173af7412
Mihai Dumitrescu <mihairazvand1@gmail.com>
2026-04-18 01:52:55 +0300

    Refactor WithdrawMenu to use generic Menu class
    Signed-off-by: Mihai Dumitrescu <mihairazvand1@gmail.com>

--- src/Rpg/menues/WithdrawMenu.cpp
+++ src/Rpg/menues/WithdrawMenu.cpp
@@ -1,9 +1,10 @@
 #include "WithdrawMenu.h"
 #include <iostream>
 
-WithdrawMenu::WithdrawMenu(sf::RenderWindow& window, const sf::Vector2f position,
-                           const sf::Vector2f size, int& crystals, int& bankBalance, int& storageCapacity)
-    :m100Button(sf::Vector2f(0, 0), sf::Vector2f(80.0f, 35.0f), "100"),
+WithdrawMenu::WithdrawMenu(const sf::Vector2f& windowSize, const sf::Vector2f& position,
+                           const sf::Vector2f& size, int& crystals, int& bankBalance, int& storageCapacity)
+    : Menu(size, position, true),
+    m100Button(sf::Vector2f(0, 0), sf::Vector2f(80.0f, 35.0f), "100"),
     m250Button(sf::Vector2f(0, 0), sf::Vector2f(80.0f, 35.0f), "250"),
     m500Button(sf::Vector2f(0, 0), sf::Vector2f(80.0f, 35.0f), "500"),
     mConfirmButton(sf::Vector2f(0, 0), sf::Vector2f(200.0f, 40.0f), "Confirm Withdrawal"),
@@ -14,14 +15,14 @@ WithdrawMenu::WithdrawMenu(sf::RenderWindow& window, const sf::Vector2f position
     if (!mFont.loadFromFile("assets/fonts/gameFont.ttf"))
         std::cerr << "Failed to load font for BankMenu!" << std::endl;
 
-    mMenuShape.setSize(size);
-    mMenuShape.setPosition(position);
-    mMenuShape.setFillColor(sf::Color(70, 70, 70, 200));
-
-    mCrystalBalance = createMessageText("Crystals: " + std::to_string(crystals), 175.f);
-    mBalance = createMessageText("Bank Balance: "  + std::to_string(bankBalance), 100.f);
-    mAmount = createMessageText("Withdraw Amount: ", 25.f);
-    mInsufficientFundsText = createMessageText("Insufficient funds in bank", 310.f);
+    mCrystalBalance = createMessageText(mFont, "Crystals: " + std::to_string(crystals),
+                                        sf::Vector2f(mMenuShape.getPosition().x + 60.f, mMenuShape.getPosition().y + 175.f));
+    mBalance = createMessageText(mFont, "Bank Balance: "  + std::to_string(bankBalance),
+                                 sf::Vector2f(mMenuShape.getPosition().x + 60.f, mMenuShape.getPosition().y + 100.f));
+    mAmount = createMessageText(mFont, "Withdraw Amount: ",
+                                sf::Vector2f(mMenuShape.getPosition().x + 60.f, mMenuShape.getPosition().y + 25.f));
+    mInsufficientFundsText = createMessageText(mFont, "Insufficient funds in bank",
+                                               sf::Vector2f(mMenuShape.getPosition().x + 60.f, mMenuShape.getPosition().y + 310.f));
 
     m100Button.setPosition(sf::Vector2f(mMenuShape.getPosition().x + 300.f,
                                         mMenuShape.getPosition().y + 25.f - 8.75f));
@@ -32,37 +33,51 @@ WithdrawMenu::WithdrawMenu(sf::RenderWindow& window, const sf::Vector2f position
     mConfirmButton.setPosition(sf::Vector2f(mMenuShape.getPosition().x + 75.f, mMenuShape.getPosition().y + 310.f));
     mBackButton.setPosition(sf::Vector2f(mMenuShape.getPosition().x + 450.f, mMenuShape.getPosition().y + 310.f));
 
-    m100Button.setCallback([&]() {
-        withdrawAmount(100, bankBalance);
+    m100Button.setCallback([this]() {
+        withdrawAmount(100);
+        updateTexts();
     });
 
-    m250Button.setCallback([&]() {
-        withdrawAmount(250, bankBalance);
+    m250Button.setCallback([this]() {
+        withdrawAmount(250);
+        updateTexts();
     });
 
-    m500Button.setCallback([&]() {
-        withdrawAmount(500, bankBalance);
+    m500Button.setCallback([this]() {
+        withdrawAmount(500);
+        updateTexts();
     });
 
-    mConfirmButton.setCallback([&]() {
-        bankBalance -= mAmountToWithdraw;
-        crystals += mAmountToWithdraw;
+    mConfirmButton.setCallback([this]() {
+        mBankBalance -= mAmountToWithdraw;
+        mCrystals += mAmountToWithdraw;
         mAmountToWithdraw = 0;
         mConfirmShowing = false;
+
+        updateTexts();
     });
 
-    mBackButton.setCallback([&]() {
+    mBackButton.setCallback([this]() {
         mAmountToWithdraw = 0;
         mConfirmShowing = false;
         mInsufficientFunds = false;
+
+        updateTexts();
     });
+
+    updateTexts();
 }
 
 void WithdrawMenu::render(sf::RenderWindow& window) {
+    if (!mIsActive)
+        return;
+
     window.draw(mMenuShape);
+
     window.draw(mBalance);
     window.draw(mAmount);
     window.draw(mCrystalBalance);
+
     m100Button.render(window);
     m250Button.render(window);
     m500Button.render(window);
@@ -71,11 +86,15 @@ void WithdrawMenu::render(sf::RenderWindow& window) {
         mConfirmButton.render(window);
         mBackButton.render(window);
     }
+
     if (mInsufficientFunds)
         window.draw(mInsufficientFundsText);
 }
 
-void WithdrawMenu::handleClicks(const sf::Vector2f& mousePos) {
+void WithdrawMenu::handleMouseClick(const sf::Vector2f& mousePos) {
+    if (!mIsActive)
+        return;
+
     if (m100Button.isMouseOver(mousePos))
         m100Button.onClick();
     if (m250Button.isMouseOver(mousePos))
@@ -92,6 +111,9 @@ void WithdrawMenu::handleClicks(const sf::Vector2f& mousePos) {
 }
 
 void WithdrawMenu::updateHover(const sf::Vector2f& mousePos) {
+    if (!mIsActive)
+        return;
+
     m100Button.updateHover(mousePos);
     m250Button.updateHover(mousePos);
     m500Button.updateHover(mousePos);
@@ -102,7 +124,29 @@ void WithdrawMenu::updateHover(const sf::Vector2f& mousePos) {
     }
 }
 
-void WithdrawMenu::update() {
+void WithdrawMenu::update(float dt) {
+    if (!mIsActive)
+        return;
+}
+
+void WithdrawMenu::restart() {
+    mConfirmShowing = false;
+    mInsufficientFunds = false;
+}
+
+void WithdrawMenu::withdrawAmount(int amount) {
+    if (mBankBalance >= amount) {
+            mAmountToWithdraw = amount;
+            mConfirmShowing = true; // show button to confirm amount to be withdrawn
+            mInsufficientFunds = false;
+        } else {
+            mAmountToWithdraw = 0;
+            mConfirmShowing = false;
+            mInsufficientFunds = true; // show low funds error message
+        }
+}
+
+void WithdrawMenu::updateTexts() {
     if (mConfirmShowing) {
         mCrystalBalance.setString("Crystals: " + std::to_string(mCrystals) + " + " +
             std::to_string(mAmountToWithdraw));
@@ -114,32 +158,3 @@ void WithdrawMenu::update() {
         std::to_string(mStorageCapacity));
     }
 }
-
-void WithdrawMenu::restart() {
-    mConfirmShowing = false;
-    mInsufficientFunds = false;
-}
-
-void WithdrawMenu::withdrawAmount(int amount, int& bankBalance) {
-    if (bankBalance >= amount) {
-            mAmountToWithdraw = amount;
-            mConfirmShowing = true; // show button to confirm amount to be withdrawn
-            mInsufficientFunds = false;
-        } else {
-            mAmountToWithdraw = 0;
-            mConfirmShowing = false;
-            mInsufficientFunds = true; // show low funds error message
-        }
-}
-
-// DUPLICATE FUNCTION IN DEPOSITMENU.CPP
-sf::Text WithdrawMenu::createMessageText(std::string string, float height) {
-    sf::Text output;
-    output.setFont(mFont);
-    output.setCharacterSize(16);
-    output.setFillColor(sf::Color::White);
-    output.setString(string);
-    output.setPosition(sf::Vector2f(mMenuShape.getPosition().x + 60.f, mMenuShape.getPosition().y + height));
-    return output;
-}
-
